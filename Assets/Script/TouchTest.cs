e52b1fe176062f525312f46dbfcfd73e17971abd
ellafr29 <107376176+ellafr29@users.noreply.github.com>
2023-05-20 21:20:39 +0700

    Update Touch Test
    Update untuk mengatur posisi kamera

--- Assets/Script/TouchTest.cs
+++ Assets/Script/TouchTest.cs
@@ -4,35 +4,82 @@ using UnityEngine;
 
 public class TouchTest : MonoBehaviour
 {
+    [SerializeField] GameObject visualTemplate;
+    List<GameObject> visualList = new List<GameObject>();
+    Camera cam;
+    
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
     void Update()
     {
-        if(Input.touchCount == 0)
+        // if(Input.touchCount == 0)
+        //     return;
+        
+        // var touches = Input.touches;
+
+        // while (visualList.Count < touches.Length)
+        // {
+        //     var visual = Instantiate(visualTemplate);
+        //     visualList.Add(visual);
+        // }
+
+        // foreach (var visual in visualList)
+        // {
+        //     visual.SetActive(false);   
+        // }
+
+        // for (int i = 0; i < touches.Length; i++)
+        // {
+        //     var touch = touches[i];
+        //     var visual = visualList[i];
+
+        //     switch(touch.phase)
+        //     {
+        //         case TouchPhase.Began:
+        //         case TouchPhase.Stationary:
+        //         case TouchPhase.Moved:
+        //            visual.SetActive(true);
+        //            var worldPos = cam.ScreenToWorldPoint(touch.position);
+        //            worldPos.z = 0;
+        //            visual.transform.position = worldPos;
+        //             break;     
+        //         case TouchPhase.Ended:     
+        //         case TouchPhase.Canceled:
+        //             break;     
+        //     }
+        // }
+
+        // if (Input.touchCount != 2)
+        //     return;
+
+        // var touches = Input.touches;
+
+        // var prevPos0 = touches[0].position - touches[0].deltaPosition;
+        // var prevPos1 = touches[1].position - touches[1].deltaPosition;
+        // var previousDistance = Vector2.Distance(prevPos0, prevPos1);
+        // var currentDistance = Vector2.Distance(touches[0].position, touches[1].position);
+        // var deltaDistance = currentDistance - previousDistance;
+
+        // if (cam.orthographic)
+        // {
+        //     cam.orthographicSize -= deltaDistance*0.1f;
+        //     cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 2, 15);
+        // }
+        // else
+        // {
+        //     cam.transform.position += Vector3.forward * deltaDistance*0.01f;
+        //     var z = Mathf.Clamp(cam.transform.position.z, -10, -1);
+        //     cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, z);
+        // }
+
+        if(Input.touchCount != 1)
             return;
         
-        var touches = Input.touches;
+        var touch = Input.GetTouch(0);
 
-        for (int i = 0; i < touches.Length; i++)
-        {
-            var touch = touches[i];
-
-            switch(touch.phase)
-            {
-                case TouchPhase.Began:
-                    Debug.Log("Began "+i+" "+touch.position);
-                    break;
-                case TouchPhase.Stationary:
-                    Debug.Log("Stationary "+i+" "+touch.position);
-                    break;
-                case TouchPhase.Moved:
-                    Debug.Log("Moved "+i+" "+touch.position);
-                    break;     
-                case TouchPhase.Ended:
-                    Debug.Log("Ended "+i+" "+touch.position);
-                    break;      
-                case TouchPhase.Canceled:
-                    Debug.Log("Canceled "+i+" "+touch.position);
-                    break;     
-            }
-        }
+        cam.transform.position -= (Vector3) touch.deltaPosition * 0.001f;
     }
 }
