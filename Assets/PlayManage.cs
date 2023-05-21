bd2f5cc51d6b48153868dd2d27a4883a2794303f
ellafr29 <107376176+ellafr29@users.noreply.github.com>
2023-05-21 15:42:37 +0700

    Update PlayManage.cs
    Penambahan untuk finish text dan juga score

--- Assets/PlayManage.cs
+++ Assets/PlayManage.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayManage : MonoBehaviour
 {
+    [SerializeField] GameObject finishedCanvas;
+    [SerializeField] TMP_Text finishedText;
+
+    int coin = 100;
+
     public void GameOver()
     {
-        Debug.Log("GameOver");
+        finishedText.text = "You Lost";
+        finishedCanvas.SetActive(true);
     }
 
     public void PlayerWin()
     {
-        Debug.Log("PlayerWin");
+        finishedText.text = "You Win!\nScore: "+GetScore();
+        finishedCanvas.SetActive(true);
     }
 
+    private int GetScore()
+    {
+        return coin * 10;
+    }
 }
